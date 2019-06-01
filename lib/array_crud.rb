9c213fb3ff915c79528d12f798bf6754961077f2
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-01 23:32:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,8 @@ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-array.index(index_number)
-return 
+array.index[index_number]
+return
 end
 
 def retrieve_first_element_from_array(array)
